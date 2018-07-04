62ba240efad98f764b39a5f71153d8404e794a1b
Victor Schirmann <victorino_s@outlook.com>
2018-07-04 02:35:07 +0200

    added style

--- src/LoginComponent.js
+++ src/LoginComponent.js
@@ -4,24 +4,49 @@ import axios from "axios/index";
 import _ from "lodash";
 
 const BaseLoginComponent = props => (
-    <div>
-        <label htmlFor="username">Username</label>
-        <input id="username" onChange={props.onUsernameChange} value={props.username} type="text"/>
-        <label htmlFor="password">Password</label>
-        <input id="password" onChange={props.onPasswordChange} value={props.password} type="password"/>
-        <button onClick={props.onLogin}>Login</button>
-        <p>{props.loginInfo}</p>
-        {props.isLoading ? <h3>Loading...</h3> : null}
+    <div className="container">
+        <div className="row">
+            <div className="col-sm">
+            </div>
+            <div className="col-sm">
+                <div className="div-center">
+                    <div className="content">
+                        <h3>Atlantis BackOffice Login</h3>
+                        <hr/>
+                        <div>
+                            <div className="form-group">
+                                <label htmlFor="username">Username</label>
+                                <input type="text" className="form-control" id="username" onChange={props.onUsernameChange} value={props.username} placeholder="Username" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <input type="password" className="form-control" id="password"
+                                       placeholder="Password" onChange={props.onPasswordChange} value={props.password} />
+                            </div>
+                            <button type="button" onClick={props.onLogin} className="btn btn-dark" disabled={props.isLoading}>Login {props.isLoading ?
+                                <i className="fa fa-refresh fa-spin"></i> : null}</button>
+                            <hr/>
+                            <div>{props.loginInfo ? <div className="alert alert-danger" role="alert">{props.loginInfo}</div> : null}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="col-sm">
+            </div>
+        </div>
     </div>
+
 )
 
 export default compose(
     withState('username', 'updateUsername', ""),
     withState('password', 'updatePassword', ""),
     withState('loginInfo', 'updateLoginInfo', ''),
+    withState('isLoading', 'updateIsLoading', false),
     withHandlers({
         onLogin: props => event => {
             event.preventDefault();
+            props.updateIsLoading(true);
             axios.post('http://localhost:30040/BackOfficeService.svc/login', {username: props.username, password: props.password})
                 .then((response) => {
                     if(!_.isNil(response.data)) {
@@ -33,11 +58,13 @@ export default compose(
                             props.updatePassword('');
                         }
                     }
+                    props.updateIsLoading(false);
                 })
                 .catch((error) => {
                     props.updateUsername('');
                     props.updatePassword('');
                     props.updateLoginInfo('Erreur : ' + error.response.data);
+                    props.updateIsLoading(false);
                 });
         },
         onPasswordChange: props => event => {
--- src/UserCard.js
+++ src/UserCard.js
@@ -1,48 +1,55 @@
 import React from 'react';
-import {compose, withHandlers} from 'recompose';
+import {compose} from 'recompose';
 import _ from 'lodash';
 import LinkDeviceButton from "./LinkDeviceButton";
 
 const BaseUserCard = props => (
-    <div>
-        <div>
-            <ul>
-                <li>{props.user.firstname}</li>
-                <li>{props.user.lastname}</li>
-            </ul>
-        </div>
-        <div>
-            <p>
-                <button className="btn btn-primary" type="button" data-toggle="collapse"
-                        data-target={"#userdevices-"+props.user.userId} aria-expanded="false"
-                        aria-controls={"userdevices-"+props.user.userId}>Show User Devices
-                </button>
-                <button className="btn btn-primary" type="button" data-toggle="collapse" data-target={"#freedevices-"+props.user.userId}
-                        aria-expanded="false" aria-controls={"freedevices-"+props.user.userId}>Show Free Devices
-                </button>
-            </p>
-            <div className="row">
-                <div className="col">
-                    <div className="collapse multi-collapse" id={"userdevices-"+props.user.userId}>
-                        <div className="card card-body">
-                            <ul className="list-group list-group-flush">
-                            {_.map(props.user.devices, device => <li key={device.id} className="list-group-item">Device : {device.name ? device.name : device.id} - Type : {device.deviceType}</li>)}
-                            </ul>
+    <div className="card">
+        <h5 className="card-header">{props.user.firstname} {props.user.lastname}</h5>
+        <div className="card-body">
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm">
+                        <div className="div-center">
+                            <button className="btn btn-primary" type="button" data-toggle="collapse"
+                                    data-target={"#userdevices-"+props.user.userId} aria-expanded="false"
+                                    aria-controls={"userdevices-"+props.user.userId}>Show User Devices
+                            </button>
+                        </div>
+                    </div>
+                    <div className="col-sm">
+                        <button className="btn btn-primary" type="button" data-toggle="collapse" data-target={"#freedevices-"+props.user.userId}
+                                aria-expanded="false" aria-controls={"freedevices-"+props.user.userId}>Show Free Devices
+                        </button>
+                        <div className="div-center">
                         </div>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="collapse multi-collapse" id={"freedevices-"+props.user.userId}>
-                        <div className="card card-body">
-                            <ul className="list-group list-group-flush">
-                                {_.map(props.devices, device => <li key={device.id} className="list-group-item"><LinkDeviceButton refresh={props.refresh} user={props.user} device={device}/></li>)}
-                            </ul>
+                <hr />
+                <div className="row">
+                    <div className="col">
+                        <div className="collapse multi-collapse" id={"userdevices-"+props.user.userId}>
+                            <div className="card card-body">
+                                <ul className="list-group list-group-flush">
+                                    {_.map(props.user.devices, device => <li key={device.id} className="list-group-item">Device : {device.name ? device.name : device.id} - Type : {device.deviceType}</li>)}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="collapse multi-collapse" id={"freedevices-"+props.user.userId}>
+                            <div className="card card-body">
+                                <ul className="list-group list-group-flush">
+                                    {_.map(props.devices, device => <li key={device.id} className="list-group-item"><LinkDeviceButton refresh={props.refresh} user={props.user} device={device}/></li>)}
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 );
 
 export default compose()(BaseUserCard);
